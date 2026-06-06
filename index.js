578d8e93617cb05fa4d2268dbb59a1be70979b8e
tasneem youssef <tasneemyoussef61@gmail.com>
2026-06-07 02:37:22 +0300

    Update origin link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const hardcodedUri="mongodb+srv://tasneem99_user:TanEeM463@taskmanagerdb.00upttf
 //middelware
 app.use(express.json());
 app.use(
-  cors({ origin: "https://tasneem-youssef.github.io/task-manger-frontend/login", credentials: true }),
+  cors({ origin: "https://tasneem-youssef.github.io/task-manger-frontend", credentials: true }),
 );
 //connect DB
 mongoose
