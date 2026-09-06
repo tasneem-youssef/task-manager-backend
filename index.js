c59e939e66b79bd41dc3eddf087ab8ac01a4f0d4
tasneem youssef <tasneemyoussef61@gmail.com>
2026-09-06 17:43:13 +0300

    refactor: externalize CORS origin to environment variable and remove redundant database connection middleware

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ const app = express();
 //middelware
 app.use(express.json());
 app.use(
-  cors({ origin: "https://tasneem-youssef.github.io/task-manger-frontend", credentials: true }),
+  cors({ origin: process.env.allowedconnection, credentials: true }),
 );
 
-console.log(process.env.MONGO_URI);
 //connect DB
 mongoose
   .connect(process.env.MONGO_URI)
@@ -22,35 +21,6 @@ mongoose
   .catch((error) => {
     console.log("mongoDB connection error", error);
   });
-
-let isConnected = false;
-const connecttoDatabase = async () => {
-  if (isConnected) return;
-
-  if (!hardcodedUri) {
-    console.log("CRITICAL ERROR: MONGO_URI is not defined or not loaded!");
-    throw new Error("MONGO_URI is missing");
-  }
-
-try {
-  const db = await mongoose.connect(process.env.MONGO_URI);
-  isConnected = db.connections[0].readyState;
-  console.log("connected to Atlas");
-} catch (error) {
-  console.log("Error while connecting to Atlas", error);
-  throw error;
-}}
-
-app.use(async (req, res, next) => {
-  try {
-    await connecttoDatabase();
-    next();
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Database connection failed", details: error.message });
-  }
-});
 
 //Routes
 app.use("/auth", authRoutes); //auth routes
