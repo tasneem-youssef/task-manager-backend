1a865876f74f435a7f763b332ee9990e1a5af56c
tasneem youssef <tasneemyoussef61@gmail.com>
2026-09-17 00:26:14 +0300

    update : change in cors method

--- index.js
+++ index.js
@@ -14,13 +14,7 @@ const allowedOrigins = process.env.allowedconnection
 
 app.use(
   cors({
-     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
   })
 )
